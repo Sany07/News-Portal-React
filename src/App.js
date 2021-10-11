fd4c56eb762d73bda558cbdd2dfa4a3e42dd4947
Ahasan Habib Sany <sany.baust@gmail.com>
2021-10-11 21:37:39 +0600

    assets updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import index  from './pages';
+
+import React from 'react'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-         sany
+         <index />
       </header>
     </div>
   );
