7639c2b39f3d8ef144dc32a34bc3c309e8b87426
Ahasan Habib Sany <sany.baust@gmail.com>
2021-10-23 00:17:35 +0600

    comments section done

---
+++ src/components/includes/Comments.jsx
@@ -0,0 +1,142 @@
+import React from 'react'
+
+export const Comments = () => {
+    return (
+       <>
+        <div className="media">
+        <div className="media-left">
+        <a href="#">
+        <img
+        alt="64x64"
+        className="media-object"
+        data-src="assets/img/reader_img1.jpg"
+        src="assets/img/reader_img1.jpg"
+        data-holder-rendered="true"
+        />
+        </a>
+        </div>
+        <div className="media-body">
+        <h2 className="media-heading">
+        <a href="#">Sr. Ryan</a>
+        </h2>
+        But who has any right to find
+        fault with a man who chooses to
+        enjoy a pleasure that has no
+        annoying consequences, or one
+        who avoids a pain that produces
+        no resultant pleasure?
+        <div className="entity_vote">
+        <a href="#">
+        <i
+        className="fa fa-thumbs-o-up"
+        aria-hidden="true"
+        />
+        </a>
+        <a href="#">
+        <i
+        className="fa fa-thumbs-o-down"
+        aria-hidden="true"
+        />
+        </a>
+        <a href="#">
+        <span className="reply_ic">
+        Reply
+        </span>
+        </a>
+        </div>
+        <div className="media">
+        <div className="media-left">
+        <a href="#">
+        <img
+        alt="64x64"
+        className="media-object"
+        data-src="assets/img/reader_img2.jpg"
+        src="assets/img/reader_img2.jpg"
+        data-holder-rendered="true"
+        />
+        </a>
+        </div>
+        <div className="media-body">
+        <h2 className="media-heading">
+        <a href="#">
+        Admin
+        </a>
+        </h2>
+        But who has any right to
+        find fault with a man
+        who chooses to enjoy a
+        pleasure that has no
+        annoying consequences,
+        or one who avoids a pain
+        that produces no
+        resultant pleasure?
+        <div className="entity_vote">
+        <a href="#">
+        <i
+        className="fa fa-thumbs-o-up"
+        aria-hidden="true"
+        />
+        </a>
+        <a href="#">
+        <i
+        className="fa fa-thumbs-o-down"
+        aria-hidden="true"
+        />
+        </a>
+        <a href="#">
+        <span className="reply_ic">
+        Reply{" "}
+        </span>
+        </a>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+
+        <div className="media">
+        <div className="media-left">
+        <a href="#">
+        <img
+        alt="64x64"
+        className="media-object"
+        data-src="assets/img/reader_img3.jpg"
+        src="assets/img/reader_img3.jpg"
+        data-holder-rendered="true"
+        />
+        </a>
+        </div>
+        <div className="media-body">
+        <h2 className="media-heading">
+        <a href="#">S. Joshep</a>
+        </h2>
+        But who has any right to find
+        fault with a man who chooses to
+        enjoy a pleasure that has no
+        annoying consequences, or one
+        who avoids a pain that produces
+        no resultant pleasure?
+        <div className="entity_vote">
+        <a href="#">
+        <i
+        className="fa fa-thumbs-o-up"
+        aria-hidden="true"
+        />
+        </a>
+        <a href="#">
+        <i
+        className="fa fa-thumbs-o-down"
+        aria-hidden="true"
+        />
+        </a>
+        <a href="#">
+        <span className="reply_ic">
+        Reply{" "}
+        </span>
+        </a>
+        </div>
+        </div>
+        </div>
+        </>
+    )
+}
--- src/pages/NewsDetail.jsx
+++ src/pages/NewsDetail.jsx
@@ -8,6 +8,7 @@ import { SingleNewsCard } from "../components/skeletons/SingleNewsCard";
 import { Sidebar } from "./Sidebar";
 
 import { Loading } from "../components/includes/Loading";
+import { Comments } from "../components/includes/Comments";
 class NewsDetail extends Component {
     state = {
         slug: "",
@@ -20,12 +21,17 @@ class NewsDetail extends Component {
         this.props.GetSidebarData();
         window.scrollTo(0, 0);
     }
+    
     render() {
         const { single_news: news, isLoading } = this.props;
-
+        if(news){
+            console.log('nnew', news);
+        
+        }
         if (this.state.slug) {
+            this.setState({ isLoading: true });
             this.props.getSingleNews(this.state.slug);
-            this.setState({ slug: "" });
+            this.setState({ slug: "", isLoading: false });
             window.scrollTo(0, 200);
         }
         if (isLoading === true || this.state.isLoading == true) {
@@ -98,141 +104,9 @@ class NewsDetail extends Component {
                                             <h2>Readers Comment</h2>
                                         </div>
                                         {/* entity_title */}
-                                        <div className="media">
-                                            <div className="media-left">
-                                                <a href="#">
-                                                    <img
-                                                        alt="64x64"
-                                                        className="media-object"
-                                                        data-src="assets/img/reader_img1.jpg"
-                                                        src="assets/img/reader_img1.jpg"
-                                                        data-holder-rendered="true"
-                                                    />
-                                                </a>
-                                            </div>
-                                            <div className="media-body">
-                                                <h2 className="media-heading">
-                                                    <a href="#">Sr. Ryan</a>
-                                                </h2>
-                                                But who has any right to find
-                                                fault with a man who chooses to
-                                                enjoy a pleasure that has no
-                                                annoying consequences, or one
-                                                who avoids a pain that produces
-                                                no resultant pleasure?
-                                                <div className="entity_vote">
-                                                    <a href="#">
-                                                        <i
-                                                            className="fa fa-thumbs-o-up"
-                                                            aria-hidden="true"
-                                                        />
-                                                    </a>
-                                                    <a href="#">
-                                                        <i
-                                                            className="fa fa-thumbs-o-down"
-                                                            aria-hidden="true"
-                                                        />
-                                                    </a>
-                                                    <a href="#">
-                                                        <span className="reply_ic">
-                                                            Reply{" "}
-                                                        </span>
-                                                    </a>
-                                                </div>
-                                                <div className="media">
-                                                    <div className="media-left">
-                                                        <a href="#">
-                                                            <img
-                                                                alt="64x64"
-                                                                className="media-object"
-                                                                data-src="assets/img/reader_img2.jpg"
-                                                                src="assets/img/reader_img2.jpg"
-                                                                data-holder-rendered="true"
-                                                            />
-                                                        </a>
-                                                    </div>
-                                                    <div className="media-body">
-                                                        <h2 className="media-heading">
-                                                            <a href="#">
-                                                                Admin
-                                                            </a>
-                                                        </h2>
-                                                        But who has any right to
-                                                        find fault with a man
-                                                        who chooses to enjoy a
-                                                        pleasure that has no
-                                                        annoying consequences,
-                                                        or one who avoids a pain
-                                                        that produces no
-                                                        resultant pleasure?
-                                                        <div className="entity_vote">
-                                                            <a href="#">
-                                                                <i
-                                                                    className="fa fa-thumbs-o-up"
-                                                                    aria-hidden="true"
-                                                                />
-                                                            </a>
-                                                            <a href="#">
-                                                                <i
-                                                                    className="fa fa-thumbs-o-down"
-                                                                    aria-hidden="true"
-                                                                />
-                                                            </a>
-                                                            <a href="#">
-                                                                <span className="reply_ic">
-                                                                    Reply{" "}
-                                                                </span>
-                                                            </a>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        {/* media end */}
-                                        <div className="media">
-                                            <div className="media-left">
-                                                <a href="#">
-                                                    <img
-                                                        alt="64x64"
-                                                        className="media-object"
-                                                        data-src="assets/img/reader_img3.jpg"
-                                                        src="assets/img/reader_img3.jpg"
-                                                        data-holder-rendered="true"
-                                                    />
-                                                </a>
-                                            </div>
-                                            <div className="media-body">
-                                                <h2 className="media-heading">
-                                                    <a href="#">S. Joshep</a>
-                                                </h2>
-                                                But who has any right to find
-                                                fault with a man who chooses to
-                                                enjoy a pleasure that has no
-                                                annoying consequences, or one
-                                                who avoids a pain that produces
-                                                no resultant pleasure?
-                                                <div className="entity_vote">
-                                                    <a href="#">
-                                                        <i
-                                                            className="fa fa-thumbs-o-up"
-                                                            aria-hidden="true"
-                                                        />
-                                                    </a>
-                                                    <a href="#">
-                                                        <i
-                                                            className="fa fa-thumbs-o-down"
-                                                            aria-hidden="true"
-                                                        />
-                                                    </a>
-                                                    <a href="#">
-                                                        <span className="reply_ic">
-                                                            Reply{" "}
-                                                        </span>
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        {/* media end */}
+                                        {news ? 
+                                        <Comments news={news.id}/>
+                                        :''}   
                                     </div>
                                     {/*Readers Comment*/}
                                     <div className="widget_advertisement">
