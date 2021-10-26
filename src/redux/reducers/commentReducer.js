04f41316b1b83c355e7e685f5d483927a80bb30c
Ahasan Habib Sany <sany.baust@gmail.com>
2021-10-26 13:26:16 +0600

    comment section started

--- src/redux/reducers/commentReducer.js
+++ src/redux/reducers/commentReducer.js
@@ -1,10 +1,18 @@
-import { FETCH_NEWS_COMMENT } from "../actions/actionTypes";
+import {
+    CREATE_NEWS_COMMENT,
+    FETCH_NEWS_COMMENT,
+} from "../actions/actionTypes";
 
 const data = { isLoading: true, comments: null };
 
 export default (comments = data, action) => {
     switch (action.type) {
         case FETCH_NEWS_COMMENT:
+            return {
+                comments: action.payload.data,
+                isLoading: false,
+            };
+        case CREATE_NEWS_COMMENT:
             return {
                 comments: action.payload.data,
                 isLoading: false,
